fe975541075b14b25765ea1110693045dca36276
Shiwangi Kumari <sshiwangi770@gmail.com>
2023-07-09 20:46:45 +0530

    adding sell page

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,41 +1,60 @@
-import { Link } from 'react-router-dom';
-import './Navbar.css';
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import React, { useState } from "react";
+import Popupcmp from "./popup/Popupcmp";
 
 const Navbar = () => {
-    return (
-        <div className="home">
-        <div className="nav">
-          <div className="navbar" />
-          <div className="nav-items">
-            <Link to="/find-help" className="find-help">Find Help</Link>
-            <Link to="/about" className="about">About</Link>
-            <Link to="/" className="home-link">Home</Link>
-            <Link to="/events" className="events">Events</Link>
-            <Link to="/learn" className="learn">Learn</Link>
-            <Link to="/sell-buy" className="sellbuy">Sell/Buy</Link>
-          </div>
-          <div className="authentication">
-            <div className="sign-up-button">
-              <div className="sign-up-button-child" />
-              <button className="sign-in">
-                <Link to="/sign-in">Sign in</Link>
-              </button>
-            </div>
-          </div>
-          <div className="logo">
-            <img
-              className="graduation-cap-icon"
-              alt=""
-              src="/graduation-cap@2x.png"
-            />
-            <b className="hustlershelp">
-              <span>Hustlers</span>
-              <span className="help">Help</span>
-            </b>
+  const [isOpen, setIsOpen] = useState(false);
+  function openPopup() {
+    setIsOpen((prev) => !prev);
+  }
+  return (
+    <div className="nav-section">
+      <div className="nav">
+        <div className="navbar" />
+        <div className="nav-items">
+          <Link to="/find-help" className="find-help">
+            Find Help
+          </Link>
+          <Link to="/about" className="about">
+            About
+          </Link>
+          <Link to="/" className="home-link">
+            Home
+          </Link>
+          <Link to="/events" className="events">
+            Events
+          </Link>
+          <Link to="/learn" className="learn">
+            Learn
+          </Link>
+          <Link onClick={openPopup} to="/sell-buy" className="sellbuy">
+            Sell/Buy
+          </Link>
+          {isOpen && <Popupcmp />}
+        </div>
+        <div className="authentication">
+          <div className="sign-up-button">
+            <div className="sign-up-button-child" />
+            <button className="sign-in">
+              <Link to="/sign-in">Sign in</Link>
+            </button>
           </div>
         </div>
+        <div className="logo">
+          <img
+            className="graduation-cap-icon"
+            alt=""
+            src="/graduation-cap@2x.png"
+          />
+          <b className="hustlershelp">
+            <span>Hustlers</span>
+            <span className="help">Help</span>
+          </b>
+        </div>
       </div>
-        );
-    };
-    
+    </div>
+  );
+};
+
 export default Navbar;
--- client/src/pages/Sellpage/Sell.jsx
+++ client/src/pages/Sellpage/Sell.jsx
@@ -2,7 +2,65 @@ import React from 'react'
 
 function Sell() {
   return (
-    <div>sell</div>
+    <div className='sell-section'>
+        <div className="section-heading">
+            Enter Item Details
+        </div>
+        <div className="sell-section-content-contianer">
+            <div className="sell-section-content">
+                <div className="item-container">
+                    <div className="item-name">Item Name</div>
+                    <input type="text" />
+                </div>
+                <div className="item-container">
+                    <div className="item-name">Item Description</div>
+                    <textarea name="" id="" cols="30" rows="5"></textarea>
+                </div>
+                <div className="item-container">
+                    <div className="item-name">Item Condition</div>
+                    <textarea name="" id="" cols="30" rows="5"></textarea>
+                </div>
+                <div className="item-container">
+                    <div className="item-name">Item Price</div>
+                    <div className="item-price-content">
+                        <div className="item-currency">Rs</div>
+                        <input type="Number" />
+                    </div>
+                    
+                </div>
+                <div className="item-container">
+                    <div className="item-name">Item location</div>
+                    <input type="text" />
+                </div>
+                <div className="item-container">
+                    <div className="item-name">Item State</div>
+                    <input type="text" />
+                </div>
+                <div className="item-container">
+                    <div className="item-name">Item City</div>
+                    <input type="text" />
+                </div>
+                <div className="item-container">
+                    <div className="item-name">Neighbourhood</div>
+                    <input type="text" />
+                </div>
+                <div className="item-container">
+                    <div className="item-name">Add Name</div>
+                    <input type="text" />
+                </div>
+                <div className="item-container">
+                    <div className="item-name">Add Phone Number</div>
+                    <input type="number" />
+                </div>
+                <div className="submit">
+                    Submit
+                </div>
+                
+                
+            </div>
+        </div>
+
+    </div>
   )
 }
 
