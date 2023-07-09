e795396a245e7178cef8a096797e86f632cac2be
Ankit Majee <ankitmajee.05@gmail.com>
2023-07-09 13:06:49 +0530

    Navbar added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router} from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      hello
-    </div>
+    <Router>
+      <Navbar />
+    </Router>
   );
 }
 
