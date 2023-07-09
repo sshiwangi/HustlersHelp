02884ac8f7cb14f3aec86c0d39824759b3e4b31e
Ankit Majee <ankitmajee.05@gmail.com>
2023-07-09 15:00:38 +0530

    Footer added

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import Sell from './pages/Sellpage/Sell'
 import './App.css';
 import { BrowserRouter as Router} from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Footer />
     </Router>
   );
 }
