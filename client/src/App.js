1d6c917fd2b93dea075de20534229d4ce033112e
firtysh <sm2450@it.jgec.ac.in>
2023-07-10 03:09:29 +0530

    Courses page

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Events from './pages/Events';
 import About from './pages/About';
 import Learn from './pages/Learn';
 import MentorDetails from './pages/MentorDetails';
+import Courses from './pages/Courses';
 
 function App() {
   return (
@@ -32,6 +33,7 @@ function App() {
       <Route path='/mentors' element={<MentorList />} /> 
       <Route path='/mentors/:id' element={<MentorDetails />} />
       <Route path='/learn' element={<Learn />} />
+      <Route path='/courses' element={<Courses />} />
     </Routes>
     <Footer/>
     </BrowserRouter>
--- client/src/pages/Events.jsx
+++ client/src/pages/Events.jsx
@@ -1,18 +1,49 @@
 import React from 'react'
-import {AiOutlineSearch} from 'react-icons/ai'
+import { AiOutlineSearch } from 'react-icons/ai'
+import { Link } from 'react-router-dom'
+import EventCard from '../components/EventCard'
+
+const data = [
+  {
+    name: 'Event 1',
+    description: 'Event 1 description',
+    image: 'https://picsum.photos/500/600',
+    date: '12/12/2021',
+    location: 'Location 1'
+  },
+  {
+    name: 'Event 1',
+    description: 'Event 1 description',
+    image: 'https://picsum.photos/500/600',
+    date: '12/12/2021',
+    location: 'Location 1'
+  },
+  {
+    name: 'Event 1',
+    description: 'Event 1 description',
+    image: 'https://picsum.photos/500/600',
+    date: '12/12/2021',
+    location: 'Location 1'
+  }
+]
+
 function Events() {
   return (
     <>
-    <div className='p-20'>
+      <div className='p-20'>
 
-    <div className='flex items-center justify-between'>
-      <div className='text-lg font-bold'>Upcoming Events</div>
-      <div className='flex border items-center rounded-full'><input className='rounded-full w-full h-full focus:outline-none p-3' type="text" /><AiOutlineSearch className='text-lg mx-2'/></div>
-    </div>
-    <div>
-      
-    </div>
-    </div>
+        <div className='flex items-center justify-between'>
+          <div className='text-lg font-bold'>Upcoming Events</div>
+          <div className='flex border items-center rounded-full'><input className='rounded-full w-full h-full focus:outline-none p-3' type="text" /><AiOutlineSearch className='text-lg mx-2' /></div>
+        </div>
+        <div className='flex w-full gap-3 flex-1'>
+          {data.map((event) => {
+            return <EventCard event={event} />
+          })
+          }
+
+        </div>
+      </div>
     </>
   )
 }
