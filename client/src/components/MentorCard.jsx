6547f4f05ff511f19cd72c51c1b7f3daae9a7fa6
Ankit Majee <ankitmajee.05@gmail.com>
2023-07-10 07:00:24 +0530

    Linked MentorDetails

--- client/src/components/MentorCard.jsx
+++ client/src/components/MentorCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link, Route } from "react-router-dom";
 
 function MentorCard({item}) {
     return (
@@ -8,7 +9,7 @@ function MentorCard({item}) {
                 <div className='font-bold text'>Name</div>
                 <div>Industry</div>
                 <div>Expertise</div>
-                <div className='bg-[#458EFF] w-full rounded-full p-3 text-lg text-white'>View Profile</div>
+                <Link to={`/mentors/${item}`} className='bg-[#458EFF] w-full rounded-full p-3 text-lg text-white'>View Profile</Link>
             </div>
         </>
     )
