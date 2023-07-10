6dede3d7e24dedfe7bcb6f0f584367f90cfc656a
Ankit Majee <ankitmajee.05@gmail.com>
2023-07-10 13:49:27 +0530

    EventDetails added

--- client/src/pages/EventDetails.jsx
+++ client/src/pages/EventDetails.jsx
@@ -7,15 +7,17 @@ function EventDetails(item) {
   return (
     <div>
       <div className="flex justify-center items-center">
-        <img className="rounded-full w-60 h-60" src="https://picsum.photos/500/600" alt="" />
-      </div>
+  <img className="object-cover w-64 h-40" src="https://picsum.photos/500/600" alt="" />
+</div>
+
       <div className="text-center mt-2">
         <div className="font-bold text-xl">Event Name</div>
       </div>
       <div className="shadow-md rounded-lg p-4 mt-4 mx-auto w-2/5">
-        <div className="font-bold text-lg">Description</div>
-        <div className="text-gray-500">
-          Merako Sab aata hai!
+        <div className="font-bold text-lg">Purpose</div>
+        <div className="text-gray-500 mb-1">
+        The quiz does not get terminated on the first such instance. 
+        Your face will be constantly detected and violations will be tracked and reported while the quiz is on.
         </div>
      </div>
     </div>
