07200809c05a60328c8a4b1aaf2c5a268ab2ea52
firtysh <sm2450@it.jgec.ac.in>
2023-07-10 10:55:42 +0530

    navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -11,15 +11,8 @@ const Navbar = () => {
   console.log(user);
   const [isOpen, setIsOpen] = useState(false);
   //for dropdown feature of help
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-  const handleNavHover = () => {
-    setIsDropdownOpen(true);
-  };
 
-  const handleNavLeave = () => {
-    setIsDropdownOpen(false);
-  };
   //function for popup of sell and buy feature
   function openPopup() {
     setIsOpen((prev) => !prev);
@@ -34,33 +27,26 @@ const Navbar = () => {
           <RiGraduationCapFill className="text-3xl text-[#458EFF]" />
           <div className="mx-2 text-3xl font-bold ">Hustler's <span className="text-[#458EFF]">Help</span></div>
         </div>
-        <div className="flex items-center justify-between text-xl font-semibold basis-1/2">
+        <div className="flex items-center justify-center gap-8 text-xl font-semibold basis-1/2">
           <Link to="/">Home</Link >
-          <Link to="#about">About</Link >
-          <Link onMouseEnter={handleNavHover} onMouseLeave={handleNavLeave} to="/help">
-            Help
-            {isDropdownOpen && (
-              <div style={{ width: '150px', height: '100px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', padding: '10px', borderRadius: '15px', boxShadow: '2px 2px 9px 5px rgba(151, 144, 144, 0.25)' }}>
-                <Link to="/mentors">Find Mentors</Link>
-                Find Teams
-              </div>)}
-          </Link >
+          <Link to="/mentors">Find Mentors</Link>
+          <Link to="/teams">Find Teams</Link>
           <Link to="/events">Events</Link >
           <Link to="/courses">Courses</Link >
           <Link onClick={openPopup} to='sellbuy'>Sell/Buy
-            {isOpen && <Popup />}
+          {isOpen && <Popup />}
           </Link>
-
         </div>
         {user?.isLoggedIn ? (
           <div class="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
             <span class="font-medium text-gray-600 dark:text-gray-300">JL</span>
           </div>) : (
           <Link to='signin' className="text-white p-3 font-bold rounded text-xl bg-[#458EFF]">
-            Signin
+            Sign In
           </Link>
         )
         }
+        
       </div>
 
     </div>
