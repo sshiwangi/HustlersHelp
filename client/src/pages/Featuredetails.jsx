eebfcac5724737a608333c6afdce68e6f4ef6b6e
Shiwangi Kumari <sshiwangi770@gmail.com>
2023-07-10 09:31:57 +0530

    added files

---
+++ client/src/pages/Featuredetails.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Carousel } from "@material-tailwind/react";
+import DemoCarousel from "../components/DemoCarousel";
+
+function Featuredetails() {
+  return (
+    <div className="feature-details-section">
+        <DemoCarousel />
+    </div>
+  );
+}
+
+export default Featuredetails;
